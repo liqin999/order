79550299b7a3fddedaaf9be284565f08faec85f6
liqin <1483510623@qq.com>
2018-06-24 12:53:37 +0800

    shell

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,16 @@
 <template>
   <div>
     <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="py-2" href="#">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mx-auto"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
-      </a>
+    <nav class="navbar navbar-expand-lg navbar-light">
       <a href="/" class="navbar-brand">Pizza点餐系统</a>
-      <ul class="navbar-nav">
-        <!-- <li><router-link tag="div" to="/" class="nav-link">主页</router-link></li> -->
-        <!-- <li><router-link :to="homeLink" class="nav-link">主页</router-link></li> -->
+      <ul class="nav nav-pills navbar-nav">
         <li><router-link :to="{name:'homeLink'}" class="nav-link">主页</router-link></li>
         <li><router-link :to="{name:'menuLink'}" class="nav-link">菜单</router-link></li>
         <li><router-link :to="{name:'adminLink'}" class="nav-link">管理</router-link></li>
         <li><router-link :to="{name:'aboutLink'}" class="nav-link">关于我们</router-link></li>
       </ul>
 
-      <ul class="navbar-nav ml-auto">
+      <ul class="nav navbar-nav fr">
         <li><router-link :to="{name:'loginLink'}" class="nav-link">登录</router-link></li>
         <li><router-link :to="{name:'registerLink'}" class="nav-link">注册</router-link></li>
       </ul>
@@ -32,6 +27,22 @@
     //     homeLink:'/'
     //   }
     // }
+    //导航完成之前获取数据导航完成之前获取数据
+
+    //相对于适口额位置  页面中盒子的狂傲使用相对于适口的大小相对于 max-height:90vh  
+    // div{width:90vw}
+
   }  
 
 </script>
+<style>
+   .fl{
+    float: left !important;
+   }
+   .fr{
+    float: right !important;
+   }
+   .navbar-light{
+    border-bottom: 1px solid #ddd;padding-bottom: 5px
+   }
+</style>
